220ddc56a853b1b0d28dff2155ced09454316a7a
root <root@beaglebone.localdomain>
2016-09-13 22:25:50 +0000

    parse hostname, use double quotes

--- tunnel.sh
+++ tunnel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source login.creds
 
-if [ "$(ss -trp | grep ESTAB | grep '$JUMP_BOX:ssh' | wc -l)" -eq 0 ]; then
+if [ "$(ss -trp | grep ESTAB | grep "$JUMP_BOX:ssh" | wc -l)" -eq 0 ]; then
     echo "ssh tunnel disconnected, attempting to reconnect"
     ssh -f -N -T -R2222:localhost:22 $JUMP_BOX_USER@$JUMP_BOX
 else
