1be9f659a86e31fe68f29670bcd5a5600c9b89ac
root <root@beaglebone.localdomain>
2016-09-13 22:24:15 +0000

    no hardcoding, move everything to var file

--- login.sh
+++ login.sh
@@ -2,24 +2,32 @@
 
 function do_xf_login() {
 
+    source login.creds
+
     echo "Download start page..."
-    CURL_CMD="curl -vs -c wificook -L stuntedrunt.com"
-    HTML=$(curl -vs -c wificook -L stuntedrunt.com)
+    HTML=$(curl -vs -c wificook -L "$LOGIN_CHECK_URL")
     echo "Done"
     
     HASH=$(echo $HTML | hxnormalize -x | hxselect 'input[name="hash"]' | sed -n -e "s/^.*value=['\"]\(.*\)['\"].*/\1/p")
     echo "Hash: $HASH"
     
-    source login.creds
-    
     set -x
-    curl -v -b wificook -L -X POST -H "application/x-www-form-urlencoded; charset=UTF-8" -F method=authenticate -F username="$USERNAME" -F password="$PASSWORD" -F friendlyname="$DEVICE_NAME" -F hash="$HASH" -F javascript=true https://wifilogin.xfinity.com/user_login.php 
+    curl -v -b wificook -L \
+        -X POST \
+        -H "application/x-www-form-urlencoded; charset=UTF-8" \
+        -F method=authenticate \
+        -F username="$USERNAME" \
+        -F password="$PASSWORD" \
+        -F friendlyname="$DEVICE_NAME" \
+        -F hash="$HASH" \
+        -F javascript=true \
+        https://wifilogin.xfinity.com/user_login.php
     set +x
 
 }
 
 echo -n "Checking for login..."
-CHECK_HTML=$(curl -sv stuntedrunt.com 2>&1)
+CHECK_HTML=$(curl -sv "$LOGIN_CHECK_URL" 2>&1)
 
 if [ "$(echo "$CHECK_HTML" | grep -i 'wifilogin' | wc -l)" -ne 0 ]; then
     echo "Failed. Logging in..."
